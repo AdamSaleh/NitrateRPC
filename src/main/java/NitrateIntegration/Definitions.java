8e1ea886330ff71c188ad17e575fe692d053cf01
Adam Saleh <asaleh@redhat.com>
2012-05-14 07:59:19 -0400

    cleaned upt envcheck

--- src/main/java/NitrateIntegration/Definitions.java
+++ src/main/java/NitrateIntegration/Definitions.java
@@ -10,7 +10,6 @@ package NitrateIntegration;
 * @author Jan Rusnacko (jrusnack at redhat.com)
 */
 public class Definitions {
-    
     public static final String __DISPLAY_NAME = "Nitrate Plugin";
     public static final String __ICON_FILE_NAME = "/plugin/NitrateIntegration/images/icon.png";
     public static final String __URL_NAME = "nitrate-plugin";
