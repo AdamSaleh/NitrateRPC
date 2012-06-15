54b99b4eed677a67f21d03b33d6a5c4e21db2c60
Adam Saleh <asaleh@redhat.com>
2012-06-15 12:15:32 +0200

    added env check test and restored comment

--- src/test/java/NitrateIntegration/FunctionalTest.java
+++ src/test/java/NitrateIntegration/FunctionalTest.java
@@ -10,10 +10,28 @@ import org.junit.Before;
 import org.junit.Test;
 import java.util.regex.Pattern;
 
-/**
- *
- * @author asaleh
- */
+ /**
+* To run this test, you should run project beforehand and keep it running
+* (this selenium doesn`t run project, it assumes it is running and
+* connects to localhost:8080/, so e.g. in Netbeans hit Run project and afterwards test
+* project).
+*
+* Also, you need create a job before testing, with following parameters:
+*
+* Name: selenium_test, multiconfiguration, check This build is parametrized
+* and enter file location test.xml, add one Axis Arch and values i386 and
+* x86_64, check Integration with Nitrate TCMS and fill values.
+*
+* This setting (parametrized build) will require file before building, so
+* make sure you have testng xml file to upload and that path is correct (
+* see line driver.findElement(By.name("file")).sendKeys("") below).
+*
+* Don`t forget to create Java class PrivatePassword with your username
+* and password - they are obviously not part of repo.
+* 
+*
+* @throws Exception
+*/
 public class FunctionalTest extends SeleneseTestCase{
     
     public FunctionalTest() {
@@ -122,6 +140,51 @@ public class FunctionalTest extends SeleneseTestCase{
 		verifyTrue(selenium.isTextPresent("Settings updated"));
 	}
 
+        @Test
+	public void testTest_check_env() throws Exception {
+		selenium.click("css=form[name=\"envCheck\"] > table > tbody > tr > td.setting-main > input[name=\"Submit\"]");
+		selenium.waitForPageToLoad("30000");
+		verifyTrue(selenium.isTextPresent("i386	Property checked"));
+		verifyTrue(selenium.isTextPresent("ia64	Property checked"));
+		selenium.click("xpath=(//input[@name='Submit'])[3]");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("name=value-Arch=>i386", "iasd");
+		selenium.click("css=form[name=\"envCheck\"] > table > tbody > tr > td.setting-main > input[name=\"Submit\"]");
+		selenium.waitForPageToLoad("30000");
+		verifyTrue(selenium.isTextPresent("Value is not linked with Arch."));
+		verifyTrue(selenium.isTextPresent("Available values for property Arch:"));
+		selenium.type("name=value-Arch=>iasd", "i686");
+		selenium.click("xpath=(//input[@name='Submit'])[3]");
+		selenium.waitForPageToLoad("30000");
+		verifyTrue(selenium.isTextPresent("i686	Property checked"));
+		selenium.type("name=property-Arch", "asdf");
+		selenium.click("css=form[name=\"envCheck\"] > table > tbody > tr > td.setting-main > input[name=\"Submit\"]");
+		selenium.waitForPageToLoad("30000");
+		verifyTrue(selenium.isTextPresent("Property is not linked to RTT."));
+		verifyTrue(selenium.isTextPresent("Available properties for group RTT:"));
+		selenium.type("name=property-asdf", "Arch");
+		selenium.click("css=form[name=\"envCheck\"] > table > tbody > tr > td.setting-main > input[name=\"Submit\"]");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Previous");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=Previous");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("css=img[alt=\"screenshot2010041014012.th.png\"]");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("css=img[alt=\"screenshot2010041014010.th.png\"]");
+		selenium.click("css=img[alt=\"tNGFiaQ.jpg\"]");
+		selenium.click("css=img[alt=\"th_2010_04_27-155623.png\"]");
+		selenium.click("css=div.postlinksb > div.inbox.crumbsplus > div.pagepost > p.pagelink.conl > a");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("id=main_image");
+		selenium.waitForPopUp("_blank", "30000");
+		selenium.waitForPageToLoad("30000");
+		selenium.selectWindow("null");
+		selenium.click("css=img[alt=\"thumb-20110511-08.53.02-xmonad-dzen-conky.png\"]");
+		selenium.selectWindow("null");
+		selenium.click("//table[@id='toc']/tbody/tr/td/ul/li[4]/ul/li[16]/a/span[2]");
+	}
+        
 	@After
 	public void tearDown() throws Exception {
 		selenium.stop();
